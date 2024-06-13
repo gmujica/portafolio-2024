9ea46f0b05498eb1aa97595a0a86cfad2e38feba
gmujica <gregorymujica1993@gmail.com>
2024-06-13 16:14:08 +0000

    gobal component and stack component added

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
+      title: "Currently building my CV web page",
       description: "The Inside Scoop",
       className: "md:col-span-2 md:row-span-1",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
